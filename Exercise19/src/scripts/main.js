ac5ddb89d176129cc4527220dee36f72687cd648
Ryan Bratton <tarrickryan@gmail.com>
2017-09-28 19:26:56 -0500

    Ex19

---
+++ Exercise19/src/scripts/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    var url = 'http://localhost:3001/posts'
+
+    $(document).ready(function () {
+        $('#btnLoad').on('click', loadPosts);
+        $('#btnAdd').on('click', savePost);
+        loadPosts();
+    });
+
+    function savePost(e) {
+        e.preventDefault();
+        var postObj = $('#frm').serialize();
+        $.post(url, postObj, function(){
+    });
+    loadPosts();
+    }
+
+    function loadPosts() {
+      $.get(url, function(response){
+        var html = convertResponseToHTML(response);
+        $('#main').html(html);
+    });
+   }
+
+    function convertResponseToHTML(resp) {
+        var html = '';
+
+        for (var i = 0; i < resp.length; i++) {
+            var post = resp[i];
+            var postHTML = '<div class="post">';
+            postHTML += '<img src="' + post.pic + '"/>';
+            postHTML += '<div class="content">';
+            postHTML += '<h3>' + post.name + '</h3>';
+            postHTML += '<p>' + post.body + '</p>';
+            postHTML += "</div>";
+
+            postHTML += "</div>";
+
+            html += postHTML;
+
+        }
+        return html;
+
+    }
+});

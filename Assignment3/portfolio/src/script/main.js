ae779254149fa9cb6e4107d21567af96e92a13d9
Ryan Bratton <tarrickryan@gmail.com>
2017-12-11 09:39:19 -0600

    added websites page, flexbox on gallery

--- Assignment3/portfolio/src/script/main.js
+++ Assignment3/portfolio/src/script/main.js
@@ -1,3 +1,5 @@
+// LANDING PAGE IMAGE SLIDE
+
 document.getElementById('clickLeft').addEventListener('click', myFunction);
 function myFunction() {
  document.getElementById("slideLeft").classList.add("leftAni");
@@ -22,4 +24,42 @@ document.getElementById('rightButton').addEventListener('click', myFunctionRemov
 function myFunctionRemove2() {
  document.getElementById("slideRight").classList.remove("rightAni");
  document.getElementById('rightButton').classList.toggle("buttonVisibleRight");
+}
+
+// MODAL STUFF
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
